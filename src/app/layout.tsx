be4d9105b78722dba3c0bac15a1aadf9e26b3cc5
samsks <samuel.meireles.15s@outlook.com>
2023-11-20 15:15:37 -0300

    feat:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
-  title: 'Ecommerce',
+  title: {
+    template: '%s | Context',
+    default: 'Context'
+  },
   description: 'A definir'
 }
 
 export default function RootLayout({
   children
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.variable}>{children}</body>
+    <html className={inter.variable} lang="pt-BR">
+      <body className="w-full h-full antialiased">
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
