6bcdcc497ba344ca38966de2c23a829ac1290cb6
samsks <samuel.meireles.15s@outlook.com>
2023-11-20 19:20:42 -0300

    feat: :sparkles: add button components

---
+++ src/components/button.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+interface ButtonProps {
+  children: React.ReactNode
+  className?: string
+}
+
+interface CustomButtonProps {
+  children: React.ReactNode
+  Size?: 'sm' | 'md'
+}
+
+const Button = ({ children, className = '' }: Readonly<ButtonProps>) => {
+  return (
+    <button
+      className={`rounded-lg justify-center items-center gap-2 inline-flex text-center text-base font-semibold ${className}`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export const BlurButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+  return (
+    <Button
+      className={`bg-black bg-opacity-25 text-white ${
+        Size === 'sm' ? 'h-8 p-1' : 'h-12 p-3'
+      }`}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export const SolidButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+  return (
+    <Button
+      className={`bg-black text-white ${
+        Size === 'sm' ? 'h-8 p-1' : 'h-12 p-3'
+      }`}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export const AlphaButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+  return (
+    <Button
+      className={`bg-black bg-opacity-5 text-black ${
+        Size === 'sm' ? 'h-8 p-1' : 'h-12 p-3'
+      }`}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export const OrangeButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+  return (
+    <Button
+      className={`bg-orange-400 bg-opacity-25 text-orange-400 ${
+        Size === 'sm' ? 'h-8 p-1' : 'h-12 p-3'
+      }`}
+    >
+      {children}
+    </Button>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import { IoSearch } from 'react-icons/io5'
 import { PiShoppingCartSimpleFill } from 'react-icons/pi'
 
+import { OrangeButton, SolidButton } from './button'
+
 export default function Header() {
   return (
     <div className="fixed top-0 left-0 z-50 flex items-center justify-center min-w-full border-b mb-12 border-gray-50 bg-white ">
@@ -25,13 +27,11 @@ export default function Header() {
           />
         </div>
         <div className="flex min-w-fit gap-2">
-          <button className=" h-12 p-3 bg-orange-400 bg-opacity-25 rounded-lg justify-center items-center gap-2 inline-flex text-orange-400 text-base font-semibold">
+          <OrangeButton>
             <PiShoppingCartSimpleFill className="w-6 h-6" />
             Cart
-          </button>
-          <button className="h-12 p-3 bg-black rounded-lg justify-center items-center gap-2 text-center text-white text-base font-semibold">
-            Log In/Sign Up
-          </button>
+          </OrangeButton>
+          <SolidButton>Log In/Sign Up</SolidButton>
         </div>
       </div>
     </div>
