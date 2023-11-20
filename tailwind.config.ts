3ab3e41d20177ee93aee4f10a945e57e799f68ae
samsks <samuel.meireles.15s@outlook.com>
2023-11-20 15:13:13 -0300

    feat: :lipstick: tailwind new patterns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,25 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    fontFamily: {
-      sans: 'var(--font-inter)'
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)']
+      },
+      height: {
+        's-m-header-desktop': 'calc(100vh - 73px)',
+        's-m-header-mobile': 'calc(100vh - 89px)'
+      },
+      margin: {
+        'm-m-header-desktop': '73px',
+        'm-m-header-mobile': '89px'
+      }
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' }
     }
   },
   plugins: []
