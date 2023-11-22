a220f58e607f47d56e6fd95d947581e6fb17ba1a
samsks <samuel.meireles.15s@outlook.com>
2023-11-21 23:11:36 -0300

    style: :lipstick: update button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,11 @@ interface ButtonProps {
 interface CustomButtonProps {
   children: React.ReactNode
   Size?: 'sm' | 'md'
+  className?: string
 }
+
+const sm = 'h-8 p-1 min-w-[32px]'
+const md = 'h-12 p-3 min-w-[48px]'
 
 const Button = ({ children, className = '' }: Readonly<ButtonProps>) => {
   return (
@@ -20,48 +24,62 @@ const Button = ({ children, className = '' }: Readonly<ButtonProps>) => {
   )
 }
 
-export const BlurButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+export const BlurButton = ({
+  children,
+  Size = 'md',
+  className = ''
+}: CustomButtonProps) => {
   return (
     <Button
       className={`bg-black bg-opacity-25 text-white ${
-        Size === 'sm' ? 'h-8 p-1 min-w-[32px]' : 'h-12 p-3 min-w-[48px]'
-      }`}
+        Size === 'sm' ? sm : md
+      }  ${className}`}
     >
       {children}
     </Button>
   )
 }
 
-export const SolidButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+export const SolidButton = ({
+  children,
+  Size = 'md',
+  className = ''
+}: CustomButtonProps) => {
   return (
     <Button
-      className={`bg-black text-white ${
-        Size === 'sm' ? 'h-8 p-1 min-w-[32px]' : 'h-12 p-3 min-w-[48px]'
-      }`}
+      className={`bg-black text-white ${Size === 'sm' ? sm : md} ${className}`}
     >
       {children}
     </Button>
   )
 }
 
-export const AlphaButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+export const AlphaButton = ({
+  children,
+  Size = 'md',
+  className = ''
+}: CustomButtonProps) => {
   return (
     <Button
       className={`bg-black bg-opacity-5 text-black ${
-        Size === 'sm' ? 'h-8 p-1 min-w-[32px]' : 'h-12 p-3 min-w-[48px]'
-      }`}
+        Size === 'sm' ? sm : md
+      }  ${className}`}
     >
       {children}
     </Button>
   )
 }
 
-export const OrangeButton = ({ children, Size = 'md' }: CustomButtonProps) => {
+export const OrangeButton = ({
+  children,
+  Size = 'md',
+  className = ''
+}: CustomButtonProps) => {
   return (
     <Button
       className={`bg-orange-400 bg-opacity-25 text-orange-400 ${
-        Size === 'sm' ? 'h-8 p-1 min-w-[32px]' : 'h-12 p-3 min-w-[48px]'
-      }`}
+        Size === 'sm' ? sm : md
+      }  ${className}`}
     >
       {children}
     </Button>
