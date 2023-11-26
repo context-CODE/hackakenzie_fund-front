a75aeafa96856ea599b795c2c04252a2560c09ef
samsks <samuel.meireles.15s@outlook.com>
2023-11-26 19:57:32 -0300

    feat: :sparkles: create category card

---
+++ src/components/category-card.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export function CategoryCard() {
+  return (
+    <div className="relative min-w-[373.33px] w-[373.33px] h-[184px] rounded-2xl sm:min-w-[150px] sm:w-[150px] sm:h-[75px] flex-col justify-center items-start inline-flex overflow-hidden">
+      <Image
+        src={'/temp/category-images/Lounge-Chair-Ottoman-Mahogany.png'}
+        alt=""
+        width={373.33}
+        height={184}
+        quality={100}
+      ></Image>
+      <h4
+        className="text-white text-xl sm:text-base font-semibold absolute left-6 sm:left-4"
+        style={{ textShadow: '0 0 4px rgba(0, 0, 0, 1)' }}
+      >
+        Cadeiras
+      </h4>
+    </div>
+  )
+}
