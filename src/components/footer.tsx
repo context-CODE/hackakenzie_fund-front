700f1b97be43161c70ff351394b6747bd753ee04
samsks <samuel.meireles.15s@outlook.com>
2023-11-27 01:41:07 -0300

    feat: :sparkles: static Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col min-w-full border-t border-gray-50 bg-white px-6">
+      <div className="container mx-auto max-w-7xl flex justify-center items-center py-12 sm:py-0 sm:pt-8 sm:pb-[5.75rem]">
+        <span className="text-neutral-400 text-base font-normal">
+          © Instor 2023
+        </span>
+      </div>
+    </div>
+  )
+}
